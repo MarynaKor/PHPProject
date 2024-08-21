413f29aa4cbcc1f6205c85604440c3d2c1c91b48
maryna.korovay <maryna.korovay@fadata.eu>
2024-08-21 18:58:53 +0200

    php connect

--- dbconnect.php
+++ dbconnect.php
@@ -8,6 +8,17 @@
     
     if ($conm->connect_error) {
          die("Connection failed: " . $conm->connect_error); 
-    }else{
-        echo"hello Testing";
     }
+    $sql = "SELECT * FROM Articles";
+    $result = $conm->query($sql);
+    if ($result->num_rows > 0) {
+         while($row = $result->fetch_assoc()) { 
+            echo "id: " . $row["ID"]. " - Name: " . $row["Title"]. "<br>"; 
+        } 
+    } else {
+         echo "0 results"; 
+        }
+
+    $conn->close();
+
+    ?>
